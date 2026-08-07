762d4b601cf88e5c3c52172c6803fcf7e3db9263
y7bnb <ryanqw2011@gmail.com>
2026-08-07 13:04:13 -0700

    added a chatroom (Chatroom.py)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.page_link("pages/Chatbot.py", label="**:rainbow[ChatBot]**", icon="1️⃣")
 st.page_link("pages/Temperature_Convertor.py", label="**Celsius to Fahrenheit Convertor**", icon="2️⃣")
+st.page_link("pages/Chatroom.py", label="**Chatroom**", icon="2️⃣")
