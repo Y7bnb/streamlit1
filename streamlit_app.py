24cc6f31696721d9fcb4109f04e17acf5181acc5
y7bnb <ryanqw2011@gmail.com>
2026-08-10 13:32:54 -0700

    fixed the rainbow effect in the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(":[Y7bnb's] 1st app!")
+st.title(":rainbow[Y7bnb's] 1st app!")
 st.page_link("pages/Chatbot.py", label="**:rainbow[ChatBot]**", icon="🤖")
-st.page_link("pages/Chatroom.py", label="**:[Chatroom]**", icon="💬")
+st.page_link("pages/Chatroom.py", label="**:rainbow[Chatroom]**", icon="💬")
 st.page_link("pages/Temperature_Convertor.py", label="**Celsius to Fahrenheit Convertor**", icon="🌡️", disabled=True)
