674e7b381b977ff59ad3935f7962a7ca1939cf36
y7bnb <ryanqw2011@gmail.com>
2026-08-13 09:21:11 -0700

    added RAG_System.py to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,5 @@ st.link_button("My CodeTorch account", "https://codetorch.net/en/users/Y7bnb", i
 st.title(":rainbow[Y7bnb's] 1st app!")
 st.page_link("pages/Chatbot.py", label="**:rainbow[ChatBot]**", icon="🤖")
 st.page_link("pages/Chatroom.py", label="**:rainbow[Chatroom]**", icon="💬")
+st.page_link("pages/RAG_System.py", label="**:rainbow[RAG System]**", icon="📝")
 st.page_link("pages/Temperature_Convertor.py", label="**Celsius to Fahrenheit Convertor**", icon="🌡️", disabled=True)
